5e24cfae0f5025691f8246e8620a68cd122541f7
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-11 21:06:25 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
-# your code goes here
+class Person
+    attr_reader :name, :bank_account, :happiness
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+
+end
+
